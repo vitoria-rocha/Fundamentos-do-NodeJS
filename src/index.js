b81306c04d38ea4859f445ac8a4241abad6350ee
vitoria-rocha <vitoria.rocha1206@gmail.com>
2021-10-04 12:25:16 -0400

    --Criando a estrutura do projeto

---
+++ src/index.js
@@ -0,0 +1,13 @@
+//apenas importando a funçao do express
+const { request, response } = require('express');
+const express = require ('express');
+
+const app = express();
+
+
+app.get("/",(request, response) => {
+  return response.json({message: "hello world"});  
+});
+
+//funçao listen fala pro express startar a aplicaçao
+app.listen(8080);
