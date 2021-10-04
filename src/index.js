49a16ce2b1ed64788795a434474f100f9790c5a3
vitoria-rocha <vitoria.rocha1206@gmail.com>
2021-10-04 12:46:40 -0400

    adicionando nodemon na aplicaçao

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const express = require ('express');
 const app = express();
 
 
-app.get("/",(request, response) => {
+app.get("/", (request, response) => {
   return response.json({message: "hello world"});  
 });
 
